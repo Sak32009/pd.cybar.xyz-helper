7cf81efe5e7e32c2326537a4a0aa3006eef0c92a
Sak32009 <Sak32009@users.noreply.github.com>
2024-07-19 21:01:13 +0200

    v1.0.0

---
+++ dist/sak32009-pd.cybar.xyz-helper.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name           pd.cybar.xyz helper
+// @namespace      sak32009-pqzjnpshzobzkjo
+// @version        1.0.0
+// @author         Sak32009
+// @description    pd.cybar.xyz helper
+// @license        MIT
+// @icon           https://pixeldrain.com/res/img/pixeldrain_196.png
+// @homepage       https://github.com/Sak32009/pd.cybar.xyz-helper
+// @homepageURL    https://github.com/Sak32009/pd.cybar.xyz-helper
+// @source         github:Sak32009/pd.cybar.xyz-helper
+// @supportURL     https://github.com/Sak32009/pd.cybar.xyz-helper/issues
+// @downloadURL    https://raw.githack.com/Sak32009/pd.cybar.xyz-helper/main/dist/sak32009-pd.cybar.xyz-helper.user.js
+// @updateURL      https://raw.githack.com/Sak32009/pd.cybar.xyz-helper/main/dist/sak32009-pd.cybar.xyz-helper.meta.js
+// @match          *://pixeldrain.com/u/*
+// @match          *://pixeldrain.com/l/*
+// @require        https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js
+// @require        https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js
+// @require        https://cdn.jsdelivr.net/npm/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant          GM_addStyle
+// @grant          unsafeWindow
+// @noframes
+// @updatedAT      Fri, 19 Jul 2024 19:00:02 GMT
+// ==/UserScript==
